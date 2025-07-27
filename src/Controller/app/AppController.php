5fdc5c8ab8c7c679eabb495369c8cf1c1b1abcaf
Mylène M <mylene.magny@gmail.com>
2025-07-27 12:33:35 +0200

    App : mise en place d'un login et logout

--- src/Controller/app/AppController.php
+++ src/Controller/app/AppController.php
@@ -8,16 +8,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class AppController extends AbstractController
 {
     #[Route('/app', name: 'app_index')]
+    #[IsGranted('ROLE_CONTRIBUTOR')]
     public function index(): Response
     {
         return $this->render('app/index.html.twig');
     }
 
     #[Route('/app/events', name: 'app_events')]
+    #[IsGranted('ROLE_CONTRIBUTOR')]
     public function events(EntityManagerInterface $entityManager): Response
     {
         return $this->render('app/events/index.html.twig', [
@@ -26,6 +29,7 @@ final class AppController extends AbstractController
     }
 
     #[Route('/app/events/{event}', name: 'app_events_details')]
+    #[IsGranted('ROLE_CONTRIBUTOR')]
     public function details(Event $event): Response
     {
         return $this->render('app/events/details.html.twig', [
